c508dc9dcbf45bb7f3d450593eec48e39268bc98
ShreyPatel-1008 <shreypatel4307@gmail.com>
2024-07-11 11:35:12 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,47 @@
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const restartButton = document.getElementById('restartButton');
+let oTurn;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
